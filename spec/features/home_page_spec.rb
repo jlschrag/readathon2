fd16c8c59cd638495fe3b1c25bc824d875078fa1
Joe Schrag <joeschrag@gmail.com>
2014-04-15 00:20:27 +0000

    Created homepage

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 feature 'Home page' do
-  scenario 'User views homepage'
+  scenario 'User views homepage' do
+    visit root_path
+    
+    expect(page).to have_content "Welcome"
+    expect(page).to have_title "Readathon2"
+  end
 end
